ac37132dc7931809a41bca5d0e977a10e71d302b
mininiv89 <mininiv89@yandex.ru>
2017-06-28 18:16:34 +0300

    Решена задача 3205

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3211/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task32.task3211;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.security.MessageDigest;
+
+/* 
+Целостность информации
+*/
+
+public class Solution {
+    public static void main(String... args) throws Exception {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(new String("test string"));
+        oos.flush();
+        System.out.println(compareMD5(bos, "5a47d12a2e3f9fecf2d9ba1fd98152eb")); //true
+
+    }
+
+    public static boolean compareMD5(ByteArrayOutputStream byteArrayOutputStream, String md5) throws Exception {
+        MessageDigest md = null;
+        md = MessageDigest.getInstance("md5", byteArrayOutputStream.toString());
+        return md.digest().equals(md5);
+    }
+}
