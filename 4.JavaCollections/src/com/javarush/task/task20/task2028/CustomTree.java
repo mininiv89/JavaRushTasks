0ae3c1216c39a125d62a47b0806328951401f6af
mininiv89 <mininiv89@yandex.ru>
2017-06-30 00:14:05 +0300

    Задача 2028 начал решать сначала

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -2,85 +2,29 @@ package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
 import java.util.AbstractList;
-import java.util.Collection;
 import java.util.List;
 
 /* 
 Построй дерево(1)
 */
-public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
-    Entry<String> root;
-
+public class CustomTree extends AbstractList implements Cloneable, Serializable {
     public static void main(String[] args) {
         List<String> list = new CustomTree();
         for (int i = 1; i < 16; i++) {
             list.add(String.valueOf(i));
         }
-        System.out.println("Expected 3, actual is " + ((CustomTree) list).getParent("8"));
+        //System.out.println("Expected 3, actual is " + ((CustomTree) list).getParent("8"));
         list.remove("5");
-        System.out.println("Expected null, actual is " + ((CustomTree) list).getParent("11"));
+        //System.out.println("Expected null, actual is " + ((CustomTree) list).getParent("11"));
     }
 
-
-    public void add(int index, String element) {
-        throw new UnsupportedOperationException();
-    }
-
-    public String remove(int index) {
-        throw new UnsupportedOperationException();
+    @Override
+    public Object get(int index) {
+        return null;
     }
 
     @Override
     public int size() {
         return 0;
     }
-
-    public String getParent(String s) {
-        return null;
-    }
-
-    @Override
-    public String get(int index) {
-        throw new UnsupportedOperationException();
-    }
-
-    public String set(int index, String element) {
-        throw new UnsupportedOperationException();
-    }
-
-    public List<String> subList(int fromIndex, int toIndex) {
-        throw new UnsupportedOperationException();
-    }
-
-    protected void removeRange(int fromIndex, int toIndex) {
-        throw new UnsupportedOperationException();
-    }
-
-    public boolean addAll(int index, Collection<? extends String> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    static class Entry<T> implements Serializable {
-        String elementName;
-        int lineNumber;
-        boolean availableToAddLeftChildren, availableToAddRightChildren;
-        Entry<T> parent, leftChild, rightChild;
-
-        public Entry(String elementName) {
-            this.elementName = elementName;
-            availableToAddLeftChildren = true;
-            availableToAddRightChildren = true;
-        }
-
-        void checkChildren() {
-            if (leftChild != null) availableToAddLeftChildren = false;
-            if (rightChild != null) availableToAddRightChildren = false;
-        }
-
-        boolean isAvailableToAddChildren() {
-            return availableToAddLeftChildren || availableToAddRightChildren;
-        }
-    }
-
-
 }
